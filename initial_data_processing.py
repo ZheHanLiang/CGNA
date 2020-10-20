c96a7fd635ec521ecac11cdba2f01ba4fd5ad68a
ZheHanLiang <liangzhehan@qq.com>
2020-10-20 19:50:30 +0800

    Update

--- initial_data_processing.py
+++ initial_data_processing.py
@@ -61,85 +61,88 @@ def data_processing(args):
 
     ## Calculate the data of the new graph
     time_head = time.time()
-    present_node = start_node # 当前节点识别符，从上面设置的start_node开始
-    index_edge = 0 # 边的序号
-    num_degree = 0 # 记录当前节点的degree
-    num_high_degree_edge = 0 # 记录满足degree要求的节点组成的子图中的边的数量，即可以删除的边的数量
-    node_set = set() # 记录被读取到的节点（方便后续补充中间被遗漏的孤岛节点）
-    nodes_beyond_degree = set() # 记录满足degree要求的节点
-    can_discard_list = [] # 记录可以删除的边的序号
-    present_edge_list = [] # 当前节点所连接的边的序号
-    f = open(output_new_data_dir, 'r', encoding='utf-8') # 第一次遍历文件，找出度满足要求的节点，并记录所有读取到的节点以及原图边的总数
+    present_node = start_node # The current node identifier, starting from the 'start_node'
+    index_edge = 0 # Edge index
+    num_degree = 0 # Record the degree of the current node
+    num_high_degree_edge = 0 # Record the number of edges in the sub-graph composed of nodes that meet the degree requirement, that is, the number of edges can be discarded
+    node_set = set() # Record the node that was read (to facilitate the subsequent supplement of the missing island node in the dicarding process)
+    nodes_beyond_degree = set() # Record nodes that meet degree requirement
+    can_discard_list = [] # Record the index of edges that can be discarded
+
+    ## Traverse the file for the first time
+    ## 1. find the nodes whose degree meets the requirements
+    ## 2. record all the read nodes and the total number of edges in the original graph
+    f = open(output_new_data_dir, 'r', encoding='utf-8')
     for line in f.readlines():
-        nodes = line.strip().split(' ') # 依次读入每行数据
-        if int(nodes[0])==present_node: # 还是同个点时；因为是从start_node开始记录的，所以start_node肯定是第一个被选择的节点
-            num_degree += 1 # 当前节点的degree加1
-            # present_edge_list.append(index_edge) # 把当前边加入当前节点所连接的边的list中
-        else: # 记录到下一个点的时候
-            if num_degree>=degree: # 当上一个节点的degree达到要求时
-                nodes_beyond_degree.add(present_node) # 记录该节点
-                # num_high_degree_edge += num_degree # 增加子图边的数量
-                # can_discard_list += present_edge_list # 记录可以删除的边的序号
-            num_degree = 1 # 初始化新节点的degree为1
-            present_node = int(nodes[0]) # 更新节点识别符
-            present_edge_list.clear() # 清空当前节点所连接的边的序号的list
-            # present_edge_list.append(index_edge) # 把当前边加入当前节点所连接的边的list中
-        node_set.add(int(nodes[0])) # 添加被读取到的节点
-        node_set.add(int(nodes[1])) # 添加被读取到的节点
-        index_edge += 1 # 更新当前边的序号
+        nodes = line.strip().split(' ') # Read in each row of data in turn
+        if int(nodes[0])==present_node: # Still the same node
+            num_degree += 1 # The degree of the current node plus 1
+        else: # When recording to the next node
+            if num_degree>=degree: # If the degree of the previous node reaches the requirement
+                nodes_beyond_degree.add(present_node) # Record this node
+            num_degree = 1 # Initialize the degree of the new node to 1
+            present_node = int(nodes[0]) # Update node identifier
+        node_set.add(int(nodes[0])) # Add the read node
+        node_set.add(int(nodes[1])) # Add the read node
+        index_edge += 1 # Update the index of the current edge
     f.close()
-    num_edge = index_edge # 记录原图总边数
-    index_edge = 0 # 边的序号
-    f = open(output_new_data_dir, 'r', encoding='utf-8') # 第二次遍历文件，根据度满足要求的节点确定能够删除的边的数目与序号
+    num_edge = index_edge # Record the total number of edges in the original graph
+    index_edge = 0 # The index of edge
+
+    ## Traverse the file for the second time, and determine the number and index of the edges
+    ## that can be discarded according to the nodes whose degree meets the requirements
+    f = open(output_new_data_dir, 'r', encoding='utf-8')
     for line in f.readlines():
-        nodes = line.strip().split(' ') # 依次读入每行数据
-        if nodes[0]!=nodes[1] and int(nodes[0]) in nodes_beyond_degree and int(nodes[1]) in nodes_beyond_degree: # 自连接不纳入可以删除的边
+        nodes = line.strip().split(' ') # Read in each row of data in turn
+        if nodes[0]!=nodes[1] and int(nodes[0]) in nodes_beyond_degree and int(nodes[1]) in nodes_beyond_degree: # Self-connections are not included edges that can be discarded
             can_discard_list.append(index_edge)
             num_high_degree_edge += 1
-        index_edge += 1 # 更新当前边的序号
+        index_edge += 1 # Update the index of the current edge
     f.close()
     time_tail = time.time()
     print("The num of nodes is %d, the max node is %d" %(len(node_set), max(node_set)))
     print("The num of edges is %d" % num_edge)
     print("The num of high-degree nodes is %d" % (len(nodes_beyond_degree)))
     print("The num of high-degree edges is %d" % (num_high_degree_edge))
-    num_edge_discard = int(num_edge * discard_edge_rate) # 计算一下可以删除的边是否比需要删除的多
+    num_edge_discard = int(num_edge * discard_edge_rate) # Calculate whether there are more edges that can be discarded than need to be discarded
     print("The num of edges need to discard is %d" % num_edge_discard)
-    assert num_high_degree_edge>2*num_edge_discard, "Edges aren't enough to discard!" # 验证是否有足够的边可以删除
+    assert num_high_degree_edge>2*num_edge_discard, "Edges aren't enough to discard!" # Assert that there are enough edges to discard(two times because there are two subgraphs)
     print("The num of edges after discarding is %d" % (num_edge-num_edge_discard))
     print("Edges are enough to discard!")
     print("2.Time:%.3f\n################################################"%(time_tail-time_head))
 
-    ## 随机选择出两部分要删除的边
+    ## Randomly select edges to be discarded in two graphs
     time_head = time.time()
     edge_set_1 = set()
     edge_set_2 = set()
     while len(edge_set_1)<num_edge_discard:
         rd = np.random.randint(0, len(can_discard_list))
         edge_set_1.add(can_discard_list[rd])
-        if len(edge_set_1)%10000==0: # 每10000步显示一下
+        if len(edge_set_1)%10000==0: # Show in every 10000 steps
             print("Have select %d edges to discard in G1"%(len(edge_set_1)))
     while len(edge_set_2)<num_edge_discard:
         rd = np.random.randint(0, len(can_discard_list))
-        if can_discard_list[rd] not in edge_set_1:
+        if can_discard_list[rd] not in edge_set_1: # The discarded edges cannot be the same
             edge_set_2.add(can_discard_list[rd])
-            if len(edge_set_2)%10000==0: # 有添加的话，每10000步显示一下
+            if len(edge_set_2)%10000==0:
                 print("Have select %d edges to discard in G2"%(len(edge_set_2)))
     time_tail = time.time()
     print("3.Time:%.3f\n################################################"%(time_tail-time_head))
 
-    ## 源域和目标域子图构建，这里进行共存边计算，验证构建结果是否正确
+    ## Construct the source domain and target domain subgraphs, 
+    ## where the coexistence edge calculation is performed to verify whether the construction result is correct
     time_head = time.time()
-    index_edge = 0 # 当前边的序号
-    num_com = 0 # 同时保留在两个子图中的边的数目
-    flag = 0 # 共存标识符，确定当前边是否同时保留在两个子图中
-    edges_source = "" # 源域要保留的边
-    edges_target = "" # 目标域要保留的边
-    node_set_s = set() # 源域要保留的节点
-    node_set_t = set() # 目标域要保留的节点
+    index_edge = 0 # The index of the current edge
+    num_com = 0 # The number of edges that remained in both two subgraphs
+    flag = 0 # Coexistence identifier, to determine whether the current edge remains in both subgraphs
+    edges_source = "" # The edge to remain in the source domain
+    edges_target = "" # The edge to remain in the target domain
+    node_set_s = set() # The node to remain in the source domain
+    node_set_t = set() # The node to remain in the target domain
+
     f = open(output_new_data_dir, 'r', encoding='utf-8')
     for line in f.readlines():
-        if index_edge in edge_set_1 and index_edge in edge_set_2: # 检查是否有边同时出现在两个要删除的集合中
+        if index_edge in edge_set_1 and index_edge in edge_set_2: # Check if there are edges in both sets to be discarded
             print("ERROR: Have the edge that both to discard in G1 ande G2!")
         else:
             nodes = line.strip().split(' ')
@@ -155,62 +158,37 @@ def data_processing(args):
                 node_set_t.add(int(nodes[0]))
                 node_set_t.add(int(nodes[1]))
                 flag += 1
-        if flag==2: # flag=2时说明当前边同时保留在两个子图中
+        if flag==2: # When flag=2, it means that the current edge remains in both two subgraphs
             num_com += 1
-        flag = 0 # 重置flag
-        index_edge += 1 # 切换到下一条边
-        if index_edge%100000==0:# 每10000条边显示一下
+        flag = 0 # Reset flag
+        index_edge += 1 # Switch to the next edge
+        if index_edge%100000==0: # Show in every 10000 edges
             print("%s edges have finished input" %index_edge)
-    rate = 1.0*num_com/num_edge # 计算共存率
+    rate = 1.0*num_com/num_edge # Calculate the coexistence rate
     print("The rate of common edges is %f" % rate)
     f.close()
     time_tail = time.time()
     print("4.Time:%.3f\n################################################"%(time_tail-time_head))
 
-    ## 如果有未被记录的孤岛节点，找出来，补上自连接
+    ## If there are isolated islands without accurate records, find them and make up the self-connection
     time_head = time.time()
-    # missing_list_s = []
-    # missing_list_t = []
-    # if len(node_set_s)<total_num: # 当源域子图有缺失点
-    #     nodes_list = list(node_set_s)
-    #     nodes_list.sort() # 对源域子图的节点序号进行排序，方便查找
-    #     num_missing = total_num - len(node_set) # 计算缺失节点的数目
-    #     num = 0 # 用于下面补充缺失节点用到的一个辅助变量，表示已补充的节点数量
-    #     for index, node in enumerate(nodes_list):
-    #         if index==node:
-    #             continue
-    #         if index==node-num-1:
-    #             missing_list_s.append(node-1)
-    #             num += 1
-    #     if len(set(nodes_list+missing_list_s))==total_num: print("The missing nodes of source have been all found!")
-    # if len(node_set_t)<total_num: # 当目标域子图有缺失点
-    #     nodes_list = list(node_set_t)
-    #     nodes_list.sort() # 对目标域子图的节点序号进行排序，方便查找
-    #     num_missing = total_num - len(node_set) # 计算缺失节点的数目
-    #     num = 0 # 用于下面补充缺失节点用到的一个辅助变量，表示已补充的节点数量
-    #     for index, node in enumerate(nodes_list):
-    #         if index==node:
-    #             continue
-    #         if index==node-num-1:
-    #             missing_list_t.append(node-1)
-    #             num += 1
-    #     if len(set(nodes_list+missing_list_t))==total_num: print("The missing nodes of target have been all found!")
     complete_set = set()
     for i in range(total_num): complete_set.add(i)
+    # Find out the nodes that are not connected
     missing_list_s = list(complete_set-node_set_s)
     missing_list_t = list(complete_set-node_set_t)
-    if len(missing_list_s)>0:
+    if len(missing_list_s)>0: # If there are outliers in the source domain
         for node in missing_list_s:
-            # edges_source += "{0} {0} {'weight': 1.0}\n".format(str(node)) # 添加缺失点的自连接
-            edges_source += "{0} {0} \n".format(str(node)) # 添加缺失点的自连接
-    if len(missing_list_t)>0:
+            # edges_source += "{0} {0} {'weight': 1.0}\n".format(str(node))
+            edges_source += "{0} {0} \n".format(str(node)) # Add the self-connection of missing node
+    if len(missing_list_t)>0: # If there are outliers in the target domain
         for node in missing_list_t:
-            # edges_target += "{0} {0} {'weight': 1.0}\n".format(str(node)) # 添加缺失点的自连接
-            edges_target += "{0} {0} \n".format(str(node)) # 添加缺失点的自连接
+            # edges_target += "{0} {0} {'weight': 1.0}\n".format(str(node))
+            edges_target += "{0} {0} \n".format(str(node)) # Add the self-connection of missing node
     time_tail = time.time()
     print("5.Time:%.3f\n################################################"%(time_tail-time_head))
 
-    ## 保存源域和目标域子图
+    ## Save source and target subgraphs
     f_s = open(output_source_dir, 'w', encoding='utf-8')
     f_t = open(output_target_dir, 'w', encoding='utf-8')
     f_s.write(edges_source)
